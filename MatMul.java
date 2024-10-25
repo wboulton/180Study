6793483b099019387445092263cf5123478243b8
wboulton <wboulton@purdue.edu>
2024-10-25 13:08:46 -0400

    Thread, exceptions, arrays
    No solution yet, I will work on this again later, probably not until sunday though so if someone wants to come up with the solution and add it in go for it.

---
+++ MatMul.java
@@ -0,0 +1,10 @@
+public class MatMul {
+    /*
+     * Matrix multiplication can be very cpu intensive. Write a function that recieves two 2D Integer arrays --
+     * int[][] A and int[][] B -- splits A in half from horizontally (has an odd number of rows it does not matter which half
+     * recieves the extra row) and uses two threads to execute matrix multiplication with B. Multiplication should happen in the 
+     * order A x B but you cannot assume that A and B can be validly multiplied together, so you must check the sizes before 
+     * performing the operation. If A and B cannot be multiplied together, throw a custom exception arraysNotMultiplicative.
+     * If they can be multiplied execute the operation with two threads and return the result matrix. 
+     */
+}
