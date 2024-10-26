66c9d9cbb86784b54323fb7cadfd1f4996b09ce8
William D. Boulton <wboulton@purdue.edu>
2024-10-26 09:58:58 -0400

    Update MatMul.java

--- MatMul.java
+++ MatMul.java
@@ -1,10 +1,11 @@
 public class MatMul {
     /*
-     * Matrix multiplication can be very cpu intensive. Write a function that recieves two 2D Integer arrays --
+     * Matrix multiplication can be very cpu intensive. Write a script that recieves two 2D Integer arrays --
      * int[][] A and int[][] B -- splits A in half from horizontally (has an odd number of rows it does not matter which half
      * recieves the extra row) and uses two threads to execute matrix multiplication with B. Multiplication should happen in the 
      * order A x B but you cannot assume that A and B can be validly multiplied together, so you must check the sizes before 
      * performing the operation. If A and B cannot be multiplied together, throw a custom exception arraysNotMultiplicative.
      * If they can be multiplied execute the operation with two threads and return the result matrix. 
+     * The integer arrays will be defined in a main method and you can work with them from there, you are allowed to use the main method
      */
 }
